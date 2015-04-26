0ba7fcfa8a4e5bac14d184139b76703c78f9891f
Richard Wang <zhwang@live.com.au>
2015-04-26 12:48:57 +1000

    Split from rzhw/rust-trousers@63a14dd0aab0d9ed383211123666a693e2001b7c

---
+++ src/main.rs
@@ -0,0 +1,39 @@
+extern crate trousers;
+
+use trousers::*;
+
+fn main() {
+    // TODO: Any cleaner way to write this?
+    let contextresult = TssContext::new();
+    if let Ok(context) = contextresult {
+        if let Ok(_) = context.connect() {
+            if let Ok(tpm) = context.get_tpm_object() {
+                println!("I'M IN UR TPM READING UR PCRZ (From Rust!)");
+                println!("TPM handle: {:?}", tpm.handle);
+                for i in 0..24 {
+                    if let Ok(vec) = tpm.pcr_read(i) {
+                        print!("PCR {:02}", i);
+                        for j in 0..19 {
+                            print!(" {:02x}", vec[j]);
+                        }
+                        print!("\n");
+                    }
+                }
+            } else {
+                println!("Failed to get TPM handle :(")
+            }
+        } else {
+            println!("Failed to connect :(");
+        }
+    } else {
+        println!("Failed :(");
+    }
+
+    /*
+    match blah {
+        Ok(context) => println!("Context created! {:?}", context.handle),
+        Err(e) => println!("Context failed with err {:?}", e),
+    }
+    println!("Hello world!");
+    */
+}
