ab0e810d18f77e722913f6a03085e33753163692
Richard Wang <zhwang@live.com.au>
2015-04-27 22:37:08 +1000

    Implement reset PCR screen

--- src/main.rs
+++ src/main.rs
@@ -31,6 +31,7 @@ fn show_view_pcrs(tpm: &TssTPM) -> Result<(), TssResult> {
     let button = newt::button(1, 26, "OK");
     newt::form_add_component(form, button);
     newt::run_form(form);
+    newt::form_destroy(form);
     Ok(())
 }
 
@@ -66,10 +67,60 @@ fn show_extend_pcr(tpm: &TssTPM) -> Result<(), TssResult> {
                 newt::form_add_component(form, new_text);
                 newt::form_add_component(form, button);
                 newt::run_form(form);
+                newt::form_destroy(form);
                 return Ok(());
             } else {
                 show_error("Not an integer");
             }
+        } else {
+            newt::form_destroy(form);
+            return Ok(());
+        }
+    }
+}
+
+fn show_reset_pcr(tpm: &TssTPM) -> Result<(), TssResult> {
+    loop {
+        newt::centered_window(80, 30, "Reset PCR");
+        let form = newt::form(None, None, 0);
+        let entry = newt::entry(1, 1, None, 10, 0);
+        newt::form_add_component(form, entry);
+        let ok_button = newt::button(1, 26, "OK");
+        let cancel_button = newt::button(10, 26, "Cancel");
+        newt::form_add_component(form, ok_button);
+        newt::form_add_component(form, cancel_button);
+
+        if newt::run_form(form) == ok_button {
+            let string = newt::entry_get_value(entry);
+            let parsed = string.parse::<u32>();
+
+            if let Ok(to_reset) = parsed {
+                let old_pcr_str = try!(read_pcr_as_str(tpm, to_reset));
+                let pcrs = try!(tpm.context.create_pcr_composite_info_long());
+                try!(pcrs.select_pcr_index_ex(to_reset, 1));
+                try!(tpm.pcr_reset(&pcrs));
+                let new_pcr_str = try!(read_pcr_as_str(tpm, to_reset));
+                newt::centered_window(60, 10, "Success");
+                let form = newt::form(None, None, 0);
+                let old_label = newt::label(0, 0, "Old value:");
+                let old_text = newt::label(0, 1, &*old_pcr_str);
+                let new_label = newt::label(0, 2, "New value:");
+                let new_text = newt::label(0, 3, &*new_pcr_str);
+                let button = newt::button(1, 5, "OK");
+                newt::form_add_component(form, old_label);
+                newt::form_add_component(form, old_text);
+                newt::form_add_component(form, new_label);
+                newt::form_add_component(form, new_text);
+                newt::form_add_component(form, button);
+                newt::run_form(form);
+                newt::form_destroy(form);
+                return Ok(());
+            } else {
+                show_error("Not an integer");
+            }
+        } else {
+            newt::form_destroy(form);
+            return Ok(());
         }
     }
 }
@@ -86,6 +137,7 @@ fn show_message(title: &str, message: &str) {
     let button = newt::button(1, 5, "OK");
     newt::form_add_component(form, button);
     newt::run_form(form);
+    newt::form_destroy(form);
 }
 
 fn show_menu(tpm: &TssTPM) {
@@ -99,11 +151,14 @@ fn show_menu(tpm: &TssTPM) {
         newt::listbox_append_entry(listbox, "Exit", 3);
         newt::form_add_component(form, listbox);
         newt::run_form(form);
+        newt::form_destroy(form);
         let result = newt::listbox_get_current(listbox);
         if result == 0 {
             show_view_pcrs(tpm);
         } else if result == 1 {
             show_extend_pcr(tpm);
+        } else if result == 2 {
+            show_reset_pcr(tpm);
         } else if result == 3 {
             return;
         }
@@ -121,34 +176,6 @@ fn main() {
         if let Ok(_) = context.connect() {
             if let Ok(tpm) = context.get_tpm_object() {
                 show_menu(&tpm);
-/*
-                println!("Now let's reset a PCR!");
-                let to_reset = get_input::<u32>("Pick a PCR:");
-                if let Ok(pcrs) = context.create_pcr_composite_info_long() {
-                    if let Ok(_) = pcrs.select_pcr_index_ex(to_reset, 1) {
-                        if let Ok(_) = tpm.pcr_reset(&pcrs) {
-                            println!("Reset the PCR! New PCR value:");
-                            if let Ok(vec) = tpm.pcr_read(to_reset) {
-                                print!("PCR {:02}", to_reset);
-                                for j in 0..vec.len() {
-                                    if j % 4 == 0 {
-                                        print!(" ");
-                                    }
-                                    print!("{:02x}", vec[j]);
-                                }
-                            } else {
-                                println!("Failed to read it!");
-                            }
-                        } else {
-                            println!("Failed to reset!");
-                        }
-                    } else {
-                        println!("Failed to select index!");
-                    }
-                } else {
-                    println!("Failed to create PCR composite info object!");
-                }
-                */
             } else {
                 println!("Failed to get TPM handle :(")
             }
